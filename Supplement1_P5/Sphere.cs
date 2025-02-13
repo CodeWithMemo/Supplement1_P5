3328b928dd6ca3a060fa8710b12ebf89909af04f
Guillermo <Antonioflores04@icloud.com>
2025-02-13 15:15:06 -0500

    feat: add radius field and constructor validation to Sphere

--- Supplement1_P5/Sphere.cs
+++ Supplement1_P5/Sphere.cs
@@ -1,5 +1,12 @@
 namespace Supplement1_P5;
 
 public class Sphere : Shape3D{
+
+    private double radius;
+    public Sphere(double radius)
+    {
+        Validate(radius);
+        this.radius = radius;
+    }
     
 }
