94ed50c6d1fe84d807d46ec03284c0daed6cd562
Guillermo <Antonioflores04@icloud.com>
2025-02-13 15:02:50 -0500

    test: ensure Sphere constructor does not throw for valid radius

--- Supplement1_P5.Tests/SphereTest.cs
+++ Supplement1_P5.Tests/SphereTest.cs
@@ -3,8 +3,9 @@ namespace Supplement1_P5.Tests;
 public class SphereTest
 {
     [Fact]
-    public void Test1()
+    public void ConstructorValidRadius_DoesNotThrow()
     {
-
+        var exception = Record.Exception(() => new Sphere(5));
+        Assert.Null(exception); 
     }
 }
