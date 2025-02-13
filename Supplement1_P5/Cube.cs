fc4179a5c3bb345e70beaa043c89af8903ca4179
Guillermo <Antonioflores04@icloud.com>
2025-02-13 14:27:26 -0500

    feat: add sideLength field and constructor validation to Cube

--- Supplement1_P5/Cube.cs
+++ Supplement1_P5/Cube.cs
@@ -1,5 +1,11 @@
 namespace Supplement1_P5;
 
 public class Cube : Shape3D{
+    private double sideLength;
 
+    public Cube(double sideLength)
+    {
+        Validate(sideLength);
+        this.sideLength = sideLength;
+    }
 }
