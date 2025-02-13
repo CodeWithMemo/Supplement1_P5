b01956b1579eec1c218e3f580592104d8820e259
Guillermo <Antonioflores04@icloud.com>
2025-02-13 13:52:31 -0500

    Added abstract methods for GetVolume, GetSurfaceArea, and dump

--- Supplement1_P5/Shape3D.cs
+++ Supplement1_P5/Shape3D.cs
@@ -1,6 +1,8 @@
 ﻿namespace Supplement1_P5;
 
-public class Shape3D
+abstract class Shape3D
 {
-
+    public abstract double GetVolume();
+    public abstract double GetSurfaceArea();
+    public abstract string dump(); 
 }
