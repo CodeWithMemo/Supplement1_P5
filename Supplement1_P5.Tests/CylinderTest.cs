f5c931ec00fe1fd64ca3193ca837ec722141300e
Guillermo <Antonioflores04@icloud.com>
2025-02-13 20:12:49 -0500

    test: validate Cylinder constructor throws for invalid radius or height

--- Supplement1_P5.Tests/CylinderTest.cs
+++ Supplement1_P5.Tests/CylinderTest.cs
@@ -8,4 +8,13 @@ public class CylindeTest
         var exception = Record.Exception(() => new Cylinder(5, 10));
         Assert.Null(exception);
     }
+
+    [Fact]
+        public void Constructor_InvalidRadiusOrHeight_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new Cylinder(0, 10));
+            Assert.Throws<ArgumentException>(() => new Cylinder(5, 0)); 
+            Assert.Throws<ArgumentException>(() => new Cylinder(-1, 10));
+            Assert.Throws<ArgumentException>(() => new Cylinder(5, -1)); 
+        }
 }
