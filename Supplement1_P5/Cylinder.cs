a7733e8c98cb90ffb8eea6bd1de8007340649a17
Guillermo <Antonioflores04@icloud.com>
2025-02-13 20:18:24 -0500

    feat: add radius and height fields with validation in Cylinder constructor

--- Supplement1_P5/Cylinder.cs
+++ Supplement1_P5/Cylinder.cs
@@ -2,4 +2,13 @@ namespace Supplement1_P5;
 
 public class Cylinder : Shape3D{
 
+    private double radius;
+    private double height;
+
+    public Cylinder(double radius, double height)
+    {
+        Validate(radius, height);
+        this.radius = radius;
+        this.height = height;
+    }
 }
