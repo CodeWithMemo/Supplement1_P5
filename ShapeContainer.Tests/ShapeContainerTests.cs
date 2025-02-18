17aacf3c86fba483fe9687250212f1e7afc11559
Guillermo <Antonioflores04@icloud.com>
2025-02-18 13:14:03 -0500

    Add test for Create method to ensure Shape3D is added to container

--- ShapeContainer.Tests/ShapeContainerTests.cs
+++ ShapeContainer.Tests/ShapeContainerTests.cs
@@ -3,8 +3,14 @@ namespace ShapeContainer.Tests;
 public class ShapeContainerTests
 {
     [Fact]
-    public void Test1()
+    public void CreateShouldAddShapeToContainer()
     {
+        var container = new ShapeContainer();
+        var shape = new Shape3D("Sphere");
 
+        container.Create(shape);
+
+        Assert.Equal(1, container.shapes.Count);
+        Assert.Equal("Sphere", container.Get(0).Name);
     }
 }
